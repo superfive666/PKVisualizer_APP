9eeb7f0829370a6de52b448dc08fcc4f6e449c42
wuchao <c.a.wu@avanade.com>
2018-07-06 21:56:14 +0800

    IOS-interactive-graph: SurfaceViewController placeholder -> for creating a SCI Chart Surface.

---
+++ interactive-graph/interactive-graph/View Controllers/SurfaceViewController.cs
@@ -0,0 +1,23 @@
+﻿using UIKit;
+
+namespace interactivegraph.View_Controllers
+{
+    public partial class SurfaceViewController : UIViewController
+    {
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            
+            // Perform any additional setup after loading the view, typically from a nib.
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+
+
+        }
+    }
+}
