95b07e057afd99e63e11e2b257e7a829a589d269
wuchao <c.a.wu@avanade.com>
2018-07-07 13:12:06 +0800

    IOS-interactive-graph: created Unit Test Project and added test classes. Pending unit test cases.

---
+++ interactive-graph/interactive-graph-Test/Controllers Test/GraphBaseControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions;
+using Xunit.Sdk;
+using interactivegraph.View_Controllers.Base_Controller;
+
+namespace interactive_graph_Test.Controllers_Test
+{
+    public class GraphBaseControllerTest
+    {
+        
+    }
+}
---
+++ interactive-graph/interactive-graph-Test/Controllers Test/MODAnticoagulantControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Extensions;
+using Xunit.Sdk;
+
+namespace interactive_graph_Test.Controllers_Test
+{
+    public class MODAnticoagulantControllerTest
+    {
+
+    }
+}
