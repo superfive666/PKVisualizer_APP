0aaaec1a77c83d828665d78257245e7f10ae18e2
superfive <c.a.wu@avanade.com>
2018-08-20 17:27:34 +0800

    IOS-interactive-graph: IntroductionPageViewDataSource.cs

--- interactive-graph/interactive-graph/Data Source/IntroductionPageViewDataSource.cs
+++ interactive-graph/interactive-graph/Data Source/IntroductionPageViewDataSource.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using UIKit;
+
 namespace interactivegraph.DataSource
 {
-    public class IntroductionPageViewDataSource : 
+    public class IntroductionPageViewDataSource : UIPageViewControllerDataSource
     {
-        
-        public IntroductionPageViewDataSource()
+        List<UIViewController> pageList;   
+        public IntroductionPageViewDataSource(List<UIViewController> pages)
         {
+            pageList = pages;
+        }
+
+        public override UIViewController GetNextViewController(UIPageViewController pageViewController, UIViewController referenceViewController)
+        {
+            var cur = pageList.IndexOf(referenceViewController);
+            if (cur < pageList.Count - 1)
+            {
+                return pageList[++cur];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override UIViewController GetPreviousViewController(UIPageViewController pageViewController, UIViewController referenceViewController)
+        {
+            var cur = pageList.IndexOf(referenceViewController);
+            if (cur > 0)
+            {
+                return pageList[--cur];
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
